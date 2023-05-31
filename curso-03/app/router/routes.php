89176bc7bbf9d60ce54dcfe2cac617a0666ce853
Darlley Brito <darlleybrito@gmail.com>
2023-05-31 08:28:22 -0300

    aula 52

--- curso-03/app/router/routes.php
+++ curso-03/app/router/routes.php
@@ -3,5 +3,6 @@
 return [
     "/" => 'home@index',
     "/user/create" => 'User@create',
-    "/user/[a-z0-9]+" => 'User@index'
+    "/user/[a-z0-9]+" => 'User@index',
+    "/user/[a-z0-9]+/name/[a-z]+" => 'User@show'
 ];
